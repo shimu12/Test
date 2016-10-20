1ce47efaf2cf258c0a6a7891f46448573cac18cb
root <sww345@yeah.net>
2016-10-20 17:17:45 +0800

    用runOnUiThread代替Thread和Handler

--- TestDemo/app/src/main/java/com/example/sww/testdemo/MainActivity.java
+++ TestDemo/app/src/main/java/com/example/sww/testdemo/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.sww.testdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.activity_main)
+    RelativeLayout activityMain;
+    @Bind(R.id.test)
+    TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        testRunMain();
     }
+
+    private void testRunMain()
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                test.setText("a little test");
+            }
+        });
+    }
+
+
 }
