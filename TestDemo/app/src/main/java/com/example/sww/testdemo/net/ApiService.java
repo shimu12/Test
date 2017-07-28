f158ed5192612f10858996e86fb7a8056df4ae56
shimu12 <sww345@yeah.net>
2017-07-28 10:00:23 +0800

    添加retrofit网络框架

--- TestDemo/app/src/main/java/com/example/sww/testdemo/net/ApiService.java
+++ TestDemo/app/src/main/java/com/example/sww/testdemo/net/ApiService.java
@@ -1,11 +1,16 @@
 package com.example.sww.testdemo.net;
 
+import java.util.Map;
+
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.QueryMap;
 import rx.Observable;
 
 /**
@@ -23,4 +28,22 @@ public interface ApiService {
     @FormUrlEncoded
     @POST("/TestServlet/HelloWorld")
     Observable<Response<ResponseBody>> getLogin(@Field("name") String name, @Field("password") String password);
+
+    /**
+     * 获取验证码
+     * @param map
+     * @return
+     */
+    @GET("app/auth-get.jhtml")
+    Observable<Response<ResponseBody>> getVersionCode(@QueryMap Map<String,String> map);
+
+
+    /**
+     * 挑货网登陆
+     * @param map
+     * @return
+     */
+    @GET("app/auth-login.jhtml")
+    Observable<Response<ResponseBody>> loginTiaohuo(@QueryMap Map<String,String> map);
+
 }
--- TestDemo/app/src/main/java/com/example/sww/testdemo/net/HttpMethod.java
+++ TestDemo/app/src/main/java/com/example/sww/testdemo/net/HttpMethod.java
@@ -1,10 +1,20 @@
 package com.example.sww.testdemo.net;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.example.sww.testdemo.common.Constant;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.prefs.Preferences;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -24,17 +34,20 @@ public class HttpMethod {
     private Retrofit retrofitMall;
     private Retrofit retrofitServerFFlibray;
     private static HttpMethod instance;
+    private  Context context;
 
     //构造方法私有
-    private HttpMethod() {
+    private HttpMethod(Context context) {
 
+        this.context = context;
         //手动创建一个OkHttpClient并设置超时时间
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+        builder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .cookieJar(new CookieManger(context));
 
         retrofit = new Retrofit.Builder()
                 .client(builder.build())
-                .addConverterFactory(GsonConverterFactory.create())
+//                .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .baseUrl(Constant.BASE_URL)
                 .build();
@@ -58,11 +71,80 @@ public class HttpMethod {
     }
 
 
+    public void getVersrionCode(Map<String,String> valueMap, Subscriber subscriber){
+        ApiService testService = retrofit.create(ApiService.class);
+        testService
+                .getVersionCode(valueMap)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+    }
+
+
+    /**
+     * 挑货网登陆
+     * @param userName
+     * @param valueMap
+     * @param subscriber
+     */
+    public void loginTiaohuo(String userName, Map<String,String> valueMap, Subscriber subscriber){
+
+        ApiService testService = retrofit.create(ApiService.class);
+        testService
+                .loginTiaohuo(valueMap)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+    }
+
+
+//    class SaveCookiesInterceptor implements Interceptor {
+//        @Override
+//        public Response intercept(Chain chain) throws IOException {
+//            Response originalResponse = chain.proceed(chain.request());
+//
+//            if (!originalResponse.headers("Set-Cookie").isEmpty()) {
+//                HashSet<String> cookies = new HashSet<>();
+//
+//                for (String header : originalResponse.headers("Set-Cookie")) {
+//                    cookies.add(header);
+//                }
+//
+//                Preferences.getDefaultPreferences().edit()
+//                        .putStringSet(Preferences.PREF_COOKIES, cookies)
+//                        .apply();
+//            }
+//
+//            return originalResponse;
+//        }
+//    }
+//
+//
+//    class LogginInterceptor implements Interceptor{
+//
+//        @Override
+//        public Response intercept(Chain chain) throws IOException {
+//
+//            Request.Builder builder = chain.request().newBuilder();
+//            HashSet<String> preferences = (HashSet) Preferences.getDefaultPreferences().getStringSet(Preferences.PREF_COOKIES, new HashSet<>());
+//            for (String cookie : preferences) {
+//                builder.addHeader("Cookie", cookie);
+//                Log.v("OkHttp", "Adding Header: " + cookie); // This is done so I know which headers are being added; this interceptor is used after the normal logging of OkHttp
+//            }
+//
+//            return chain.proceed(builder.build());
+//        }
+//    }
+
+
+
     //获取单例
-    public static HttpMethod getInstance() {
+    public static HttpMethod getInstance(Context context) {
         if (instance == null) {
-            instance = new HttpMethod();
+            instance = new HttpMethod(context);
         }
+
+
         return instance;
     }
 
